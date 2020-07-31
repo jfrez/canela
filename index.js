c8a24bfdaefa292af256db5a7c27319d2437867c
j frez <jfrez@Jitsi.172.20.85.12>
2020-07-31 21:16:29 +0000

    habla

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
-
 client.on('message', msg => {
-  if (msg.content.includes('comida')) {
-    msg.reply('woff');
-  }
-  if (msg.content.includes('canela')) {
-    msg.reply('woff woff');
-  }
-});
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Woff ${member}!`);
+	if (msg.content.includes('comida')) {
+		msg.reply('woff');
+	}
+	if (msg.content === ('canela')) {
+		msg.reply('woff woff:  canela habla ,  comida');
+	}
+	if (msg.content.startsWith('canela habla')) {
+		if (msg.channel.type !== 'text') return;
+		const voiceChannel =msg.member.voice.channel;
+
+		if (!voiceChannel) {
+			return msg.reply('grrrr');
+		}
+		var song = msg.content.split(' ');
+		song = song[2];
+		voiceChannel.join().then(connection => {
+			var stream;
+			if(typeof song != "undefined")
+			 stream = ytdl('https://www.youtube.com/watch?v='+song, { filter: 'audioonly' });
+			else
+			 stream = ytdl('https://www.youtube.com/watch?v=lTRiuFIWV54', { filter: 'audioonly' });
+			const dispatcher = connection.play(stream);
+			msg.reply('<#'+voiceChannel.id+'> woff');
+			dispatcher.on('finish', () => voiceChannel.leave());
+		});
+	}
 });
 
-client.login('');
+
+client.on('message', message => {
+	if (message.content === '!play') {
+		if (message.channel.type !== 'text') return;
+
+		const voiceChannel = message.member.voice.channel;
+
+		if (!voiceChannel) {
+			return message.reply('please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=lTRiuFIWV54', { filter: 'audioonly' });
+			const dispatcher = connection.play(stream);
+
+			dispatcher.on('finish', () => voiceChannel.leave());
+		});
+	}
+});
+
+client.login('token');
