14a745c3bcc438752ac97eeb579dfe2b8e6a31e1
j frez <jfrez@Jitsi.172.20.85.12>
2020-07-31 23:27:11 +0000

    muerde

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on('message', msg => {
 	if (msg.content.includes('comida')) {
 		msg.reply('woff');
 	}
-	if (msg.content === ('canela')) {
+	if (msg.content === ('canela help')) {
 		msg.reply('woff woff:  canela habla ,  comida');
 	}
 	if (msg.content.startsWith('canela habla')) {
@@ -25,34 +25,27 @@ client.on('message', msg => {
 		voiceChannel.join().then(connection => {
 			var stream;
 			if(typeof song != "undefined")
-			 stream = ytdl('https://www.youtube.com/watch?v='+song, { filter: 'audioonly' });
+				stream = ytdl('https://www.youtube.com/watch?v='+song, { filter: 'audioonly' });
 			else
-			 stream = ytdl('https://www.youtube.com/watch?v=lTRiuFIWV54', { filter: 'audioonly' });
+				stream = ytdl('https://www.youtube.com/watch?v=lTRiuFIWV54', { filter: 'audioonly' });
 			const dispatcher = connection.play(stream);
 			msg.reply('<#'+voiceChannel.id+'> woff');
 			dispatcher.on('finish', () => voiceChannel.leave());
 		});
 	}
-});
-
-
-client.on('message', message => {
-	if (message.content === '!play') {
-		if (message.channel.type !== 'text') return;
-
-		const voiceChannel = message.member.voice.channel;
-
-		if (!voiceChannel) {
-			return message.reply('please join a voice channel first!');
+	if (msg.content.startsWith("canela muerde")) {
+		if (msg.member.hasPermission("KICK_MEMBERS")) {
+			 if (msg.mentions.users.first()) {
+				 msg.mentions.members.first().kick("grrr").then((member) => {
+					msg.channel.send("grrr " + member.displayName );
+				}).catch(() => {
+					msg.channel.send("zzzzzzz");
+				});
+			}
+		}else{
+				 msg.reply('woff ¬¬');
 		}
-
-		voiceChannel.join().then(connection => {
-			const stream = ytdl('https://www.youtube.com/watch?v=lTRiuFIWV54', { filter: 'audioonly' });
-			const dispatcher = connection.play(stream);
-
-			dispatcher.on('finish', () => voiceChannel.leave());
-		});
 	}
 });
 
-client.login('token');
+                client.login('token');
